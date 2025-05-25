768916fc2854a7b3e2e23671d44f83dbd35fa434
nguyen123 <dinh2671@gmail.com>
2025-05-25 09:46:57 +0700

    Add project files.

---
+++ lesson2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void xep_loai(float diem) {
+	printf("nhap diem: ");
+	scanf_s("%f", &diem);
+	while (diem > 10.0 || diem < 0.0) {
+		printf("nhap lai diem: ");
+		scanf_s("%f", &diem);
+	}
+	if (diem >= 8.0) printf("gioi");
+	else if (diem >= 6.5 && diem < 8.0) printf("kha");
+	else if (diem >= 5.0 && diem < 6.5) printf("trung binh");
+	else printf("yeu");
+}
+void main(void) {
+	float diem=0;
+	xep_loai(diem);
+}
+
